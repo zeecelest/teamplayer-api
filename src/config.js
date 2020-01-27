aa8019dcdc7ffa6b349d406ca6ccb7f0e825d331
Zee Celest <zeecelest@gmail.com>
2020-01-27 15:46:33 -0500

    updating database

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.REACT_APP_API_KEY || 'dummy-api-token',
-  DB_URL: process.env.DB_URL || 'postgresql://zeecelest@localhost/team_player'
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://zeecelest@localhost/team_player'
 };
